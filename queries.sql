ac1bfe8afced92971c1f5e230b7de0aad6b5cffb
medoim3-ux <medoim3@gmail.com>
2026-03-10 15:51:31 +0300

    Add SQL queries for gene-disease analysis

--- queries.sql
+++ queries.sql
@@ -1,1 +1,30 @@
--- SQL queries for testing and analysis will be added here later.
+-- Show all gene-disease associations
+SELECT
+    g.symbol,
+    d.disease_name,
+    a.association_type,
+    a.evidence_level,
+    a.source_db
+FROM gene_disease_associations a
+JOIN genes g ON a.gene_id = g.gene_id
+JOIN diseases d ON a.disease_id = d.disease_id;
+
+-- Show all genes associated with Lung Cancer
+SELECT
+    g.symbol,
+    g.full_name,
+    d.disease_name
+FROM gene_disease_associations a
+JOIN genes g ON a.gene_id = g.gene_id
+JOIN diseases d ON a.disease_id = d.disease_id
+WHERE d.disease_name = 'Lung Cancer';
+
+-- Show all diseases associated with TP53
+SELECT
+    g.symbol,
+    d.disease_name,
+    a.association_type
+FROM gene_disease_associations a
+JOIN genes g ON a.gene_id = g.gene_id
+JOIN diseases d ON a.disease_id = d.disease_id
+WHERE g.symbol = 'TP53';
